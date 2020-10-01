ad4e05f67da14adf025cc46049038714c496da2d
unknown <abdinajibmohamud@gmail.com>
2020-10-01 11:24:41 -0700

    updated with port and environment on get

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const server = express()
 server.get ('/', (req, res) => {
-    res.status(200).json({message: 'hello', port: `${process.env.NODE_ENV}`})
+    res.status(200).json({message: 'hello', port: `${process.env.PORT}`, environment: `${process.env.NODE_ENV}`})
 })
 
 module.exports = server;

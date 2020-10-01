72ac5edc29f1ff56eba5a7653edf6871871b339b
unknown <abdinajibmohamud@gmail.com>
2020-10-01 10:57:43 -0700

    added port

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const server = express()
 server.get ('/', (req, res) => {
-    res.status(200).json({message: 'hello world '})
+    res.status(200).json({message: 'hello', port: `${process.env.PORT}`})
 })
 
 module.exports = server;

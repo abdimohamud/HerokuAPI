200903ee561240fc6711ea1f8d26b7407e8e05a6
unknown <abdinajibmohamud@gmail.com>
2020-10-13 15:25:15 -0700

    inital setup

--- data/dbConfig.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-const knex = require("knex");
-const knexConfig = require("../knexfile");
-
-module.exports = knex(knexConfig[process.env.DB_ENV || "development"]);
--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const server = require("./server");
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
-});
